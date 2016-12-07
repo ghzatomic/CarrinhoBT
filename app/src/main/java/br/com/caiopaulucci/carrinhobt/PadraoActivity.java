5f0fff968bc3fff3c73bf54d038a8e33c3d2abf6
Caio Rodrigo Paulucci <caiopaulucci@finchsolu>
2016-12-06 23:27:16 -0200

    Alteracoes

---
+++ app/src/main/java/br/com/caiopaulucci/carrinhobt/PadraoActivity.java
@@ -0,0 +1,18 @@
+package br.com.caiopaulucci.carrinhobt;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class PadraoActivity extends Activity {
+
+
+	public AlertDialog.Builder buildMessageOK(Context context , String titulo ,String mensagem){
+		AlertDialog.Builder dialogo = new AlertDialog.Builder(context);
+		dialogo.setTitle(titulo);
+		dialogo.setMessage(mensagem);
+		dialogo.setNeutralButton("Ok", null);
+		return dialogo;
+	}
+	
+}
